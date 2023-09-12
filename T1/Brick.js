dc93469689dd06e6630a6a495e1302e7ea65485a
williancsmelo <melo.williancs@gmail.com>
2023-09-11 21:48:21 -0300

    created bricks

--- T1/Brick.js
+++ T1/Brick.js
@@ -7,19 +7,26 @@ const getColor = (row) =>
     3: "yellow",
     4: "blue",
     5: "pink",
-    6: "green",
+    6: "palegreen",
   }[row] || "white");
 
 export class Brick {
   box = null;
   broken = false;
-  static height = 5;
-  static width = 20;
-  constructor(plane, row) {
-    const boxGeometry = new T.BoxGeometry(this.width, this.height);
-    const boxMaterial = new T.MeshBasicMaterial({ color: getColor(row) });
+  static height = 8;
+  constructor(plane, row, width) {
+    const boxGeometry = new T.BoxGeometry(width, Brick.height, 1);
+    const edges = new T.EdgesGeometry(boxGeometry);
+    const line = new T.LineSegments(
+      edges,
+      new T.LineBasicMaterial({ color: "black", linewidth: 2 })
+    );
+    const boxMaterial = new T.MeshBasicMaterial({
+      color: getColor(row),
+    });
     this.box = new T.Mesh(boxGeometry, boxMaterial);
     this.box.position.z = 0;
+    this.box.add(line);
     plane.add(this.box);
   }
 }
--- T1/Hitter.js
+++ T1/Hitter.js
@@ -1,24 +1,21 @@
 import * as T from "three";
 
 export default class Hitter {
+  static platform;
 
-    static platform;
+  constructor(plane, width = 100, height = 5, color = 0x00ff00) {
+    const platformGeometry = new T.BoxGeometry(width, height, 1);
+    const platformMaterial = new T.MeshBasicMaterial({ color });
+    const platform = new T.Mesh(platformGeometry, platformMaterial);
+    plane.add(platform);
 
-    constructor(plane, width = 100, height = 5, color = 0x00ff00) {
-        const platformGeometry = new T.BoxGeometry(width, height, 1);
-        const platformMaterial = new T.MeshBasicMaterial({ color });
-        const platform = new T.Mesh(platformGeometry, platformMaterial);
-        plane.add(platform);
+    this.platform = platform;
 
-        console.log("plataform : ", plane, platform)
+    // Posicione a plataforma
+    platform.position.set(0, -300, 0);
+  }
 
-        this.platform = platform;
-
-        // Posicione a plataforma
-        platform.position.set(0, -300, 0);
-    }
-
-    setPosition(newPosition){
-        this.platform.position.x = newPosition
-    }
+  setPosition(newPosition) {
+    this.platform.position.x = newPosition;
+  }
 }
