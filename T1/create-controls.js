49dfd8e514d494c1249b7a2000a6f0f77bda243e
williancsmelo <melo.williancs@gmail.com>
2023-09-11 22:31:23 -0300

    refactor create controls

--- T1/create-controls.js
+++ T1/create-controls.js
@@ -1,52 +1,21 @@
-export function pauseGame(gamePause){
-    gamePause = !gamePause;
-    
-    if (gamePause) {
-        document.querySelector("body").classList.remove("invisible-cursor")
-    } else {
-        document.querySelector("body").classList.add("invisible-cursor")
+export function createControls(state) {
+  const handleKey = (key) => {
+    if (key === " ") {
+      state.pause = !state.pause;
+      if (state.pause)
+        document.querySelector("body").classList.remove("invisible-cursor");
+      else document.querySelector("body").classList.add("invisible-cursor");
     }
 
-    return gamePause
-}
+    if (key === "Enter") {
+      state.fullScreen = !state.fullScreen;
+      if (state.fullScreen)
+        document.querySelector("canvas").requestFullscreen();
+      else document.exitFullscreen();
+    }
+  };
 
-export function toggleFullscreen(){
-    isFullscreen() ? exitFullscreen() : startFullscreen();
+  document.addEventListener("keydown", (event) => {
+    handleKey(event.key);
+  });
 }
-
-function isFullscreen() {
-    return (
-        document.fullscreenElement ||
-        document.mozFullScreenElement ||
-        document.webkitFullscreenElement ||
-        document.msFullscreenElement
-    );
-}
-  
-function startFullscreen() {
-const canvas = document.querySelector('canvas'); // Substitua 'seu-canvas' pelo ID do seu elemento canvas
-
-if (canvas.requestFullscreen) {
-    canvas.requestFullscreen();
-    canvas.req
-} else if (canvas.mozRequestFullScreen) { // Firefox
-    canvas.mozRequestFullScreen();
-} else if (canvas.webkitRequestFullscreen) { // Chrome, Safari e Opera
-    canvas.webkitRequestFullscreen();
-} else if (canvas.msRequestFullscreen) { // Internet Explorer/Edge
-    canvas.msRequestFullscreen();
-}
-}
-
-function exitFullscreen() {
-if (document.exitFullscreen) {
-    document.exitFullscreen();
-} else if (document.mozCancelFullScreen) { // Firefox
-    document.mozCancelFullScreen();
-} else if (document.webkitExitFullscreen) { // Chrome, Safari e Opera
-    document.webkitExitFullscreen();
-} else if (document.msExitFullscreen) { // Internet Explorer/Edge
-    document.msExitFullscreen();
-}
-}
-
--- T1/index.js
+++ T1/index.js
@@ -1,16 +1,12 @@
 import * as T from "three";
-import {
-  initRenderer,
-  onWindowResize,
-  initDefaultBasicLight,
-} from "../libs/util/util.js";
+import { initRenderer, initDefaultBasicLight } from "../libs/util/util.js";
 import { createCamera } from "./create-camera.js";
 import { createPlane } from "./create-plane.js";
 import { createBricks } from "./create-bricks.js";
 import { createHitter } from "./create-hitter.js";
-import {pauseGame, toggleFullscreen} from "./create-controls.js"
+import { createControls } from "./create-controls.js";
+import { isFullscreen } from "./utils.js";
 
-let gamePause = false;
 const renderer = initRenderer();
 const scene = new T.Scene();
 initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
@@ -19,19 +15,14 @@ const plane = createPlane(scene);
 const bricks = createBricks(plane);
 const hitter = createHitter(plane);
 
+const state = {
+  fullScreen: isFullscreen(),
+};
+createControls(state);
 
 render();
-
-/* GAME CONTROLS */
-
-document.addEventListener('keydown', (event) => {
-  if (event.key === ' ') gamePause = pauseGame(gamePause)
-  else if(event.key === 'Enter') toggleFullscreen();
-});
-
-
 function render() {
-  if(!gamePause){
+  if (!state.pause) {
     renderer.render(scene, camera); // Render scene
   }
   requestAnimationFrame(render);
