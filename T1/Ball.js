ac02cb264d1dc349a108d1da68e80442e021f89e
williancsmelo <melo.williancs@gmail.com>
2023-09-13 19:50:04 -0300

    fixed collision

--- T1/Ball.js
+++ T1/Ball.js
@@ -1,51 +1,58 @@
-import * as T from "three";
-import { ball, totalAxleY } from "./config/constants.js";
-
+import * as T from 'three'
+import { ball, totalAxleY } from './config/constants.js'
 
 export class Ball {
-	static height = 8;
-	broken = false;
-	baseSpeed = 2;
-	bb;
-	movementVector = new T.Vector3(0, 1, 0).normalize()
-  object;
-  constructor(plane, width = 100, height = 5, color = "red") {
-    const geometry = new T.SphereGeometry( 6, 32, 16 );
-    const material = new T.MeshBasicMaterial( { color: 0xffff00 } );
-    this.object = new T.Mesh(geometry, material);
-		this.bb = new T.Box3().setFromObject(this.object);
-    this.object.position.y = ball.initialPositionY;
-    this.object.position.x = ball.initialPositionX;
-    this.object = this.object;
-    plane.add(this.object);
+  static height = 8
+  broken = false
+  baseSpeed = 2
+  movementVector = new T.Vector3(0, 1, 0)
+  object = new T.Mesh(
+    new T.SphereGeometry(6, 32, 16),
+    new T.MeshBasicMaterial({ color: 'red' })
+  )
+  bb = new T.Box3().setFromObject(this.object)
+
+  constructor(plane, width = 100, height = 5) {
+    this.object.position.y = ball.initialPositionY
+    this.object.position.x = ball.initialPositionX
+    plane.add(this.object)
   }
-  
-  
-  resetBall(positionX = 0){
+
+  resetBall(positionX = 0) {
     this.movementVector = new T.Vector3(0, 1, 0).normalize()
-    this.setPosition(positionX);
+    this.setPosition(positionX)
   }
-  
+
   updateBall(controls) {
-    this.object.position.x += this.movementVector.x * this.baseSpeed;
-    this.object.position.y += this.movementVector.y * this.baseSpeed;
+    this.object.position.x += this.movementVector.x * this.baseSpeed
+    this.object.position.y += this.movementVector.y * this.baseSpeed
 
-		this.bb = new T.Box3().setFromObject(this.object);
+    this.bb = new T.Box3().setFromObject(this.object)
 
-    if(this.object.position.y * -1 > totalAxleY)  {
+    if (this.object.position.y * -1 > totalAxleY) {
       controls.setIsStarted(false)
     }
   }
 
-  setPosition(x = ball.initialPositionX, y = ball.initialPositionY){
-    this.object.position.x = x;
-    this.object.position.y = y;
+  setPosition(x = ball.initialPositionX, y = ball.initialPositionY) {
+    this.object.position.x = x
+    this.object.position.y = y
+  }
+
+  collide(normalVector) {
+    this.movementVector = this.movementVector.sub(
+      normalVector
+        .clone()
+        .multiplyScalar(2 * this.movementVector.clone().dot(normalVector))
+    )
+    if (this.movementVector.y < 0) this.movementVector.y = 0.25
+    this.movementVector.normalize()
   }
 }
 
 // function createBBHelper(bb, color, plataform) {
 //     let helper = new T.Box3Helper( bb, color );
 //     plataform.add( helper );
-    
+
 //     return helper;
 //   }
--- T1/Hitter.js
+++ T1/Hitter.js
@@ -1,47 +1,75 @@
-import * as T from "three";
+import * as T from 'three'
 
 export default class Hitter {
-  static platform;
+  static platform
   plane
-  width;
-  stop = false;
+  width
+  stop = false
+  colliding = false
 
   pieces = [
     { object: null, bb: null, normal: new T.Vector3(0, 1, 0) }, // --_--
-    { object: null, bb: null, normal: new T.Vector3(Math.cos(Math.PI / 3), Math.sin(Math.PI / 3), 0) }, // ---_-
-    { object: null, bb: null, normal: new T.Vector3(Math.cos(Math.PI / 3), Math.sin(Math.PI / 3), 0) }, // -_---
-    { object: null, bb: null, normal: new T.Vector3(Math.cos(Math.PI / 6), Math.sin(Math.PI / 6), 0) }, // ----_
-    { object: null, bb: null, normal: new T.Vector3(Math.cos(Math.PI / 6), Math.sin(Math.PI / 6), 0) } // _----
-  ];
+    {
+      object: null,
+      bb: null,
+      normal: new T.Vector3(Math.cos(Math.PI / 3), Math.sin(Math.PI / 3), 0)
+    }, // ---_-
+    {
+      object: null,
+      bb: null,
+      normal: new T.Vector3(-Math.cos(Math.PI / 3), Math.sin(Math.PI / 3), 0)
+    }, // -_---
+    {
+      object: null,
+      bb: null,
+      normal: new T.Vector3(Math.cos(Math.PI / 6), Math.sin(Math.PI / 6), 0)
+    }, // ----_
+    {
+      object: null,
+      bb: null,
+      normal: new T.Vector3(-Math.cos(Math.PI / 6), Math.sin(Math.PI / 6), 0)
+    } // _----
+  ]
 
-  constructor(plane, width = 100, height = 5, color = 0x00ff00, positionY = -350) {
-    const eachPieceWidth = width / 5;
-    const initialPos = [0, eachPieceWidth, -eachPieceWidth, 2 * eachPieceWidth, -2 * eachPieceWidth];
-    this.plane = plane;
-    this.width = width;
-    this.events();
-
+  constructor(
+    plane,
+    width = 100,
+    height = 5,
+    color = 0x00ff00,
+    positionY = -350
+  ) {
+    const eachPieceWidth = width / 5
+    const initialPos = [
+      0,
+      eachPieceWidth,
+      -eachPieceWidth,
+      2 * eachPieceWidth,
+      -2 * eachPieceWidth
+    ]
+    this.plane = plane
+    this.width = width
+    this.events()
 
     this.pieces.forEach((piece, index) => {
-      let object = this._createPiece(eachPieceWidth, height, piece.color);
+      let object = this._createPiece(eachPieceWidth, height, piece.color)
 
-      piece.object = object;
-      piece.bb = this._createCollisionBox(object, "yellow");
-    });
+      piece.object = object
+      piece.bb = this._createCollisionBox(object, 'yellow')
+    })
 
-    this.platform = this._createPiece(width, height);
-    this.platform.position.set(0, positionY, 0);
-    this.plane.add(this.platform);
+    this.platform = this._createPiece(width, height)
+    this.platform.position.set(0, positionY, 0)
+    this.plane.add(this.platform)
 
     // Adiciona e posiciona na cena
     this.pieces.forEach((piece, index) => {
-      this.platform.add(piece.object);
-      piece.object.position.x = initialPos[index];
+      this.platform.add(piece.object)
+      piece.object.position.x = initialPos[index]
     })
   }
 
   setPosition(newPosition) {
-    this.platform.position.x = newPosition;
+    this.platform.position.x = newPosition
   }
 
   updateHitter() {
@@ -51,62 +79,58 @@ export default class Hitter {
   }
 
   checkCollisions(ball) {
-    const collision = this.pieces.find(piece => {
-      return piece.bb.intersectsBox(ball.bb)
-    })
-    if (!collision) return
-    const angle = ball.movementVector.angleTo(collision.normal);
-    let angleInDegree = angle * (180 / Math.PI)
-    if (ball.movementVector.x > 0) angleInDegree -= 90
-    ball.movementVector = this.angleToVector(angleInDegree);
-    if (ball.movementVector.y < 0) ball.movementVector.y = 0.08
-    ball.movementVector.normalize()
+    const collision = this.pieces.find(piece => piece.bb.intersectsBox(ball.bb))
+    if (!collision) {
+      this.colliding = false
+      return
+    }
+    if (this.colliding) return
+    this.colliding = true
+    ball.collide(collision.normal)
   }
 
   angleToVector(angleInDegree) {
-    var anguloEmRadianos = angleInDegree * (Math.PI / 180);
-    var x = Math.cos(anguloEmRadianos);
-    var y = Math.sin(anguloEmRadianos);
-    var z = 0;
+    var anguloEmRadianos = angleInDegree * (Math.PI / 180)
+    var x = Math.cos(anguloEmRadianos)
+    var y = Math.sin(anguloEmRadianos)
+    var z = 0
 
-    return new T.Vector3(x, y, z);
+    return new T.Vector3(x, y, z)
   }
 
   _createCollisionBox(object) {
-    const bbPlat = new T.Box3().setFromObject(object);
+    const bbPlat = new T.Box3().setFromObject(object)
 
-    return bbPlat;
+    return bbPlat
   }
 
+  _createPiece(eachPieceWidth, height = 100, color = 'green') {
+    const platformGeometry = new T.BoxGeometry(eachPieceWidth, height, 1)
+    const platformMaterial = new T.MeshBasicMaterial({ color })
+    const platform = new T.Mesh(platformGeometry, platformMaterial)
 
-  _createPiece(eachPieceWidth, height = 100, color = "green") {
-    const platformGeometry = new T.BoxGeometry(eachPieceWidth, height, 1);
-    const platformMaterial = new T.MeshBasicMaterial({ color });
-    const platform = new T.Mesh(platformGeometry, platformMaterial);
-
-    return platform;
+    return platform
   }
 
   events() {
-    const onMouseMove = (event) => {
+    const onMouseMove = event => {
       const screenWidth = this.plane.geometry.parameters.width / 2
-      const totalWidth = window.innerWidth / 2;
+      const totalWidth = window.innerWidth / 2
       const newPosition = event.clientX - totalWidth
-      if (Math.abs(newPosition) < (screenWidth - (this.width / 2))) {
+      if (Math.abs(newPosition) < screenWidth - this.width / 2) {
         this.setPosition(newPosition)
         //if (!isStarted) ball.setPosition(newPosition)
       }
-    };
+    }
 
-    window.addEventListener("mousemove", onMouseMove);
+    window.addEventListener('mousemove', onMouseMove)
   }
-
 }
 
 function createBBHelper(bb, color, piece) {
   // Create a bounding box helper
-  let helper = new T.Box3Helper(bb, color);
-  piece.add(helper);
+  let helper = new T.Box3Helper(bb, color)
+  piece.add(helper)
 
-  return helper;
+  return helper
 }
