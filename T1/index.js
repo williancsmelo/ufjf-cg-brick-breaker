a8b77cb27f55a1bc94b54fe1c0728cc6e873f0ed
SavioChermont <saviochwt@gmail.com>
2023-09-13 17:56:24 -0300

    feat: create game score

--- T1/index.js
+++ T1/index.js
@@ -8,6 +8,7 @@ import { createBall } from "./create-ball.js";
 import { createControls } from "./create-controls.js";
 import { isFullscreen } from "./utils.js";
 
+
 const renderer = initRenderer();
 const scene = new T.Scene();
 initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
@@ -17,6 +18,7 @@ let bricks = createBricks(plane);
 const ball = createBall(plane);
 const controls = createControls(isFullscreen());
 const hitter = createHitter(plane, ball, controls.isStarted);
+let breakedBricks = [] // Vetor para armazenar bricks quebradas - Exemplo: [{rowIndex: 2, columnIndex: 2}, ..., {rowIndex: 1, columnIndex: 0}]
 render();
 
 function render() {
@@ -43,12 +45,15 @@ function deleteBrick(brick) {
 
 function checkColissionWithBrick() {
   let stop;
-  bricks.forEach((brickRow) => {
+  bricks.forEach((brickRow, columnIndex) => {
+
     stop = false
-    brickRow.forEach(brick => {
+    brickRow.forEach((brick, rowIndex) => {
       if (!stop && brick.checkCollisions(ball)) {
         deleteBrick(brick)
         stop = true;
+        breakedBricks.push({ rowIndex, columnIndex }) // Guarda a brick quebrada no vetor
+        updateScore();
       }
     })
   })
@@ -68,6 +73,12 @@ function restartGame(plane) {
   })
 
   bricks = createBricks(plane);
+  breakedBricks = [];
+  updateScore();
+}
 
+function updateScore() {
+  let score = breakedBricks.length;
+  document.querySelector("#score").innerHTML = "Pontuação: " + score
 }
 
