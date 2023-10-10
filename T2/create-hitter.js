210b56f9cf146f0f928ab3e0102ff3fe71716b67
Igor Infingardi <46407523+infingardi@users.noreply.github.com>
2023-10-10 23:22:10 +0000

    excluindo o hitter antigo

--- T2/create-hitter.js
+++ T2/create-hitter.js
@@ -1,13 +1,7 @@
-//import Hitter, { Hitter2 } from "./Hitter.js"
-import { Hitter2 } from "./Hitter.js"
+import { Hitter } from "./Hitter.js"
 import { hitter } from "./config/constants.js"
 
-// export function createHitter(plane) {
-//   const brick = new Hitter(plane, hitter.width, hitter.height, hitter.color, hitter.positionY);
-//   return brick;
-// }
-
-export function createHitter2(plane) {
-  const brick = new Hitter2(plane, hitter.color, hitter.positionY);
+export function createHitter(plane) {
+  const brick = new Hitter(plane, hitter.color, hitter.positionY);
   return brick;
 }
