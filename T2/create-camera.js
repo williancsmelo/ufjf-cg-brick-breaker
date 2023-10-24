b43b97017be8778b473adeba919420ce00fec593
williancsmelo <melo.williancs@gmail.com>
2023-10-24 19:36:39 -0300

    camera zoom

--- T2/create-camera.js
+++ T2/create-camera.js
@@ -1,26 +1,26 @@
-import * as T from "three";
+import * as T from 'three'
 
 export const createCamera = (scene, renderer) => {
   const camera = new T.PerspectiveCamera(
     80,
     window.innerWidth / window.innerHeight,
     0.1,
-    1000,
-  );
-  camera.position.set(5, 5, 125);
-  camera.up.set(0, 1, 0);
-  camera.lookAt(0, 2, -10);
-  scene.add(camera);
+    1000
+  )
+  camera.position.set(5, 5, 117)
+  camera.up.set(0, 1, 0)
+  camera.lookAt(0, 2, -10)
+  scene.add(camera)
 
   window.addEventListener(
-    "resize",
+    'resize',
     function () {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(window.innerWidth, window.innerHeight);
+      camera.aspect = window.innerWidth / window.innerHeight
+      camera.updateProjectionMatrix()
+      renderer.setSize(window.innerWidth, window.innerHeight)
     },
-    false,
-  );
+    false
+  )
 
-  return camera;
-};
+  return camera
+}
